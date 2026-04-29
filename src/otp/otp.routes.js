5ce39831d14d672c8f563cf07605b684b9da7330
sandeeprana95 <sandeeprana385@gmail.com>
2026-04-29 12:37:52 +0530

    Revert "work on otp"
    This reverts commit aa2ce42cfd0be839629fad7f5562172c7c3d1420.

--- src/otp/otp.routes.js
+++
@@ -1,9 +0,0 @@
-import express from "express";
-import { sendOtp, verifyOtp } from "./otp.controller.js";
-
-const router = express.Router();
-
-router.post("/send-otp", sendOtp);
-router.post("/verify-otp", verifyOtp);
-
-export default router;
